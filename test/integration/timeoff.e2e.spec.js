d7e662bcbfdf6d0fd2f6cf3ba59e693ed71408c8
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2026-04-25 14:30:04 +0500

    Update timeoff.e2e.spec.js

--- test/integration/timeoff.e2e.spec.js
+++ test/integration/timeoff.e2e.spec.js
@@ -1,1 +1,24 @@
 
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+describe("TimeOff Flow", () => {
+    it("create request flow", async () => {
+        const res = await (0, supertest_1.default)("http://localhost:3000")
+            .post("/api/timeoff/request")
+            .send({
+            employeeId: "1",
+            locationId: "L1",
+            startDate: "2026-01-01",
+            endDate: "2026-01-03",
+        });
+        expect(res.status).toBe(201);
+    });
+    it("approve request flow", async () => {
+        const res = await (0, supertest_1.default)("http://localhost:3000").post("/api/timeoff/1/approve");
+        expect([200, 201]).toContain(res.status);
+    });
+});
