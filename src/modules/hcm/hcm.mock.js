7288f99c5dc1121be92982c4af293c85dc8707dc
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2026-04-25 14:39:27 +0500

    Update hcm.mock.js

--- src/modules/hcm/hcm.mock.js
+++ src/modules/hcm/hcm.mock.js
@@ -1,1 +1,22 @@
 
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HcmMock = void 0;
+class HcmMock {
+    static validateSuccess() {
+        return { valid: true, remainingBalance: 10 };
+    }
+    static validateFailure() {
+        return { valid: false, remainingBalance: 0 };
+    }
+    static timeout() {
+        throw new Error("HCM timeout");
+    }
+    static batchResponse() {
+        return [
+            { employeeId: "1", locationId: "L1", balance: 10 },
+            { employeeId: "2", locationId: "L1", balance: 8 },
+        ];
+    }
+}
+exports.HcmMock = HcmMock;
