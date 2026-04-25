e6b186a62afdc783ee533e66413ceb5e3e956f13
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2026-04-25 14:41:58 +0500

    Update timeoff.controller.js

--- src/modules/timeoff/timeoff.controller.js
+++ src/modules/timeoff/timeoff.controller.js
@@ -1,1 +1,58 @@
 
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TimeOffController = void 0;
+const common_1 = require("@nestjs/common");
+const timeoff_service_1 = require("./timeoff.service");
+let TimeOffController = class TimeOffController {
+    constructor(service) {
+        this.service = service;
+    }
+    create(dto) {
+        return this.service.create(dto);
+    }
+    approve(id) {
+        return this.service.approve(id);
+    }
+    reject(id) {
+        return this.service.reject(id);
+    }
+};
+exports.TimeOffController = TimeOffController;
+__decorate([
+    (0, common_1.Post)("request"),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], TimeOffController.prototype, "create", null);
+__decorate([
+    (0, common_1.Post)(":id/approve"),
+    __param(0, (0, common_1.Param)("id")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", void 0)
+], TimeOffController.prototype, "approve", null);
+__decorate([
+    (0, common_1.Post)(":id/reject"),
+    __param(0, (0, common_1.Param)("id")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", void 0)
+], TimeOffController.prototype, "reject", null);
+exports.TimeOffController = TimeOffController = __decorate([
+    (0, common_1.Controller)("timeoff"),
+    __metadata("design:paramtypes", [timeoff_service_1.TimeOffService])
+], TimeOffController);
